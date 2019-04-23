80326f3884d906fee0cc7b591c6a91d87eeb3b21
Albert Juhé Lluveras <contact@albertjuhe.com>
2019-04-23 13:08:06 +0200

    Init repo

---
+++ bin/pre-push-hook.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+console.log( '\nBy contributing to this project, you license the materials you contribute ' +
+	'under the GNU General Public License v2 (or later). All materials must have ' +
+	'GPLv2 compatible licenses — see .github/CONTRIBUTING.md for details.\n\n' );
